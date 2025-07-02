749201c97bfac0e64d5d4377e52a6ac3f3ee496e
gmnguney <ega32@sfu.ca>
2025-07-02 23:59:14 +0300

    added boilerplate code for script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const converter = document.getElementById("converter");
+const amount = document.getElementById("amount");
+const from = document.getElementById("from");
+const to = document.getElementById("to");
+const result = document.getElementById("result");
